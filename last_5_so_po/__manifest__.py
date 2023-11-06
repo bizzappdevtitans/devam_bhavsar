3cf3ca3143a920d7d11c9c1cda9c1e8f30422b16
Devam bhavsar <DEB@BAD.com>
2023-11-06 11:39:18 +0530

    [WIP][T00447]last_5_so_po:Added last 5 SO

---
+++ last_5_so_po/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "last_5_so_&_po",
+    "version": "15.0.1.0.0",
+    "depends": ["base", "purchase", "product", "sale_management", "sale"],
+    "author": "Odoo Community Association (OCA),bizzappdev",
+    "website": "https://www.bizzappdev.com",
+    "category": "Administration",
+    "summary": "",
+    "data": [
+        "views/product_template_views.xml",
+        "views/product_views.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
