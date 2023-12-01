d1e0129272679e48bf790e04f57a43167c5930f3
DEB <DEB@BAD.com>
2023-12-01 13:08:20 +0530

    [WIP][T00447]last_five_so_po:Changed author

--- last_five_so_po/__manifest__.py
+++ last_five_so_po/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "last_five_so_po",
     "version": "15.0.1.0.0",
     "depends": ["purchase", "sale_management"],
-    "author": "Odoo Community Association (OCA),bizzappdev",
+    "author": "bizzappdev",
     "website": "https://www.bizzappdev.com",
     "category": "Administration",
     "summary": "Module to show last five SO and PO of a product",
